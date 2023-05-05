2e4df539a39b3eb1d0b82631d5e59bf5673a66cc
MSreeN <khyathimnsree369@gmail.com>
2023-05-05 19:03:49 +0530

    prototype bean scope

--- src/main/java/com/games/learnspringframework/Exercise/ExerciseMain.java
+++ src/main/java/com/games/learnspringframework/Exercise/ExerciseMain.java
@@ -4,6 +4,7 @@ package com.games.learnspringframework.Exercise;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,8 +15,10 @@ class ClassA{
 
 //if we don't mention lazy then it is called eagerly loading, which means the bean of classB will be loaded right after the application started and even before context was created.
 //If it is lazy loading then bean of ClassB will only be created right before some one makes use of that bean.
+
 @Component
 @Lazy
+@Scope(value = "prototype")
 class ClassB{
 
   private ClassA classA;
@@ -34,7 +37,13 @@ public class ExerciseMain {
       //If it is lazy loading the bean of ClassB will be created after this line.
       //If it is not lazy loading then bean of ClassB will be created before this line is printed into console.
       System.out.println("-------------------");
-      applicationContext.getBean(ClassB.class);
+      System.out.println(applicationContext.getBean(ClassB.class));
+      System.out.println("-------------------");
+      System.out.println(applicationContext.getBean(ClassB.class));
+      System.out.println("-------------------");
+      System.out.println(applicationContext.getBean(ClassB.class));
+      System.out.println("-------------------");
+      System.out.println(applicationContext.getBean(ClassB.class));
     }
   }
 }

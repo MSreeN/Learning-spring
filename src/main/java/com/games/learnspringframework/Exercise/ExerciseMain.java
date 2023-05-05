c6db757345769749478fd9c0f9b0879aa4ed1e53
MSreeN <khyathimnsree369@gmail.com>
2023-05-05 16:21:34 +0530

    lazy loading

--- src/main/java/com/games/learnspringframework/Exercise/ExerciseMain.java
+++ src/main/java/com/games/learnspringframework/Exercise/ExerciseMain.java
@@ -3,12 +3,35 @@ package com.games.learnspringframework.Exercise;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+class ClassA{
+
+
+} 
+
+@Component
+@Lazy
+class ClassB{
+
+  private ClassA classA;
+  ClassB(ClassA classA){
+    this.classA = classA;
+    System.out.println("from classB constructor "+ classA);
+  }
+
+}
 
 @ComponentScan
 public class ExerciseMain {
   public static void main(String[] args) {
     try(var applicationContext = new AnnotationConfigApplicationContext(ExerciseMain.class)){
-      System.out.println("output " + applicationContext.getBean(BusinessCalculationsService.class).findMax());
+      // System.out.println("output " + applicationContext.getBean(BusinessCalculationsService.class).findMax());
+
+      System.out.println("-------------------");
+      // applicationContext.getBean(ClassB.class);
     }
   }
 }

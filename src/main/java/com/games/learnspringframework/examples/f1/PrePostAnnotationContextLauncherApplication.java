54c38ed96551cc073c80a2f51132f222560e76ea
MSreeN <khyathimnsree369@gmail.com>
2023-05-06 16:38:57 +0530

    post construct

--- src/main/java/com/games/learnspringframework/examples/f1/PrePostAnnotationContextLauncherApplication.java
+++ src/main/java/com/games/learnspringframework/examples/f1/PrePostAnnotationContextLauncherApplication.java
@@ -1,7 +1,13 @@
 package com.games.learnspringframework.examples.f1;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+
+// @Lazy
 @Component
 class SomeClass{
   private SomeDependency someDependency;
@@ -10,17 +16,28 @@ class SomeClass{
     this.someDependency = someDependency;
   }
 
-  
+  @PostConstruct
+  public void initialize(){
+    someDependency.getSample();
+  }
 }
 
 @Component
 class SomeDependency{
 
+  public void getSample() {
+    System.out.println("------From getSample method called form SomeClass---------");
+  }
+
 }
 
-
+@ComponentScan
 public class PrePostAnnotationContextLauncherApplication {
   public static void main(String[] args) {
+    try(var context = new AnnotationConfigApplicationContext(PrePostAnnotationContextLauncherApplication.class);){
+      System.out.println("----------------------");
+      context.getBean(SomeClass.class);
+    }
     
   }
 }

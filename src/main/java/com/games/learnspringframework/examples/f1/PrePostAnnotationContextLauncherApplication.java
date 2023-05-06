486265be3d72791ebd7d24f5ad507fee12075637
MSreeN <khyathimnsree369@gmail.com>
2023-05-06 16:31:44 +0530

    post construct and pre destroy

--- src/main/java/com/games/learnspringframework/examples/f1/PrePostAnnotationContextLauncherApplication.java
+++ src/main/java/com/games/learnspringframework/examples/f1/PrePostAnnotationContextLauncherApplication.java
@@ -1,5 +1,24 @@
 package com.games.learnspringframework.examples.f1;
 
+import org.springframework.stereotype.Component;
+
+@Component
+class SomeClass{
+  private SomeDependency someDependency;
+  public SomeClass(SomeDependency someDependency){
+    System.out.println("From the SomeClass constructor");
+    this.someDependency = someDependency;
+  }
+
+  
+}
+
+@Component
+class SomeDependency{
+
+}
+
+
 public class PrePostAnnotationContextLauncherApplication {
   public static void main(String[] args) {
     

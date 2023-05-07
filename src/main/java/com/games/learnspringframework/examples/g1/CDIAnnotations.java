0a4a0426e397d90ac8366fcf1d28adf669d51ebf
MSreeN <khyathimnsree369@gmail.com>
2023-05-07 19:01:57 +0530

    CDI annotations

--- src/main/java/com/games/learnspringframework/examples/g1/CDIAnnotations.java
+++ src/main/java/com/games/learnspringframework/examples/g1/CDIAnnotations.java
@@ -1,16 +1,20 @@
 package com.games.learnspringframework.examples.g1;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
-
+//Instead of component we can use Named
 @Named
 class Sample{
-  @Autowired
+  // @Autowired
+  //Instead of autowired we can use inject annotation
+  @Inject
   private Needed needed;
 }
 
-
+@Named
 class Needed{
 
 }
@@ -18,6 +22,10 @@ class Needed{
 
 public class CDIAnnotations {
   public static void main(String[] args) {
-    
+    try(var context = new AnnotationConfigApplicationContext(CDIAnnotations.class)){
+      for(String bean: context.getBeanDefinitionNames()){
+        System.out.println(bean);
+      }
+    }
   }
 }
